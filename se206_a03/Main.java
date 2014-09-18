fcde8488b401ca4ad445ad82b9fd0e06dee63a40
Davidjin0418 <bjin718@aucklanduni.ac.nz>
2014-09-18 18:09:37 +1200

    add Main.java to be the entry of the app,add a static file to be edited in other three frames

---
+++ se206_a03/Main.java
@@ -0,0 +1,25 @@
+package se206_a03;
+
+import java.io.File;
+
+import javax.swing.JFrame;
+
+public class Main {
+	//create a static method to edit or play.(for other three frames to use)
+	 public static File file;
+	 public static void main(String[] args){
+		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	           public void run() {
+	               createGUI();
+	           }
+	       });
+	    }
+		public static void createGUI(){
+			MainFrame mainFrame=new MainFrame();
+			mainFrame.setTitle("Welcome to VAMIX");
+			mainFrame.setSize(500, 400);
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			mainFrame.setVisible(true);
+			mainFrame.setResizable(false);
+		}
+}
