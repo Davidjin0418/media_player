c55fb99ed5dcfd6f1851665306e61f678f64c6ef
Davidjin0418 <bjin718@aucklanduni.ac.nz>
2014-09-22 13:12:51 +1200

    add vlcj library,can perform some functions

--- se206_a03/PlayFrame.java
+++ se206_a03/PlayFrame.java
@@ -1,10 +1,5 @@
 package se206_a03;
 
-
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-
 import javax.imageio.ImageIO;
 import javax.swing.AbstractButton;
 import javax.swing.JFrame;
@@ -13,62 +8,131 @@ import javax.swing.border.EmptyBorder;
 
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 
-import java.awt.Button;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
 
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
-public class PlayFrame extends JFrame {
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+public class PlayFrame extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private final JPanel panel = new JPanel();
-	//problem loading video,need to fix it on a Linux machine;
-	//private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+	private JPanel panel;
+	// problem loading video,need to fix it on a Linux machine;
+	private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 	private JButton btnPause;
 	private JButton btnForward;
 	private JButton btnBackward;
 	private JButton btnPlay;
+	private JButton btnStrip;
+	private JButton btnMute;
+	private JButton btnOverlay;
+	private JButton btnReplace;
 
 	/**
 	 * Create the frame.
 	 */
 	public PlayFrame() {
-		//mediaPlayerComponent =new EmbeddedMediaPlayerComponent();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		//setContentPane(mediaPlayerComponent);
-		//mediaPlayerComponent.getMediaPlayer().playMedia(Main.file.getAbsolutePath());
-		contentPane.setLayout(null);
-		panel.setBounds(0, 0, this.getWidth(), this.getHeight()-100);
-		contentPane.add(panel);
-		
-		btnPlay = new JButton("Play",new ImageIcon(PlayFrame.class.getResource("/se206_a03/png/play43.png")));
-		btnPlay.setBounds(10,this.getHeight()-100, 70, 70);
-		btnPlay.setVerticalTextPosition(AbstractButton.BOTTOM);
-	    btnPlay.setHorizontalTextPosition(AbstractButton.CENTER);
-		contentPane.add(btnPlay);
-		
-		btnPause = new JButton("Pause",new ImageIcon(PlayFrame.class.getResource("/se206_a03/png/pause15.png")));
-		btnPause.setVerticalTextPosition(AbstractButton.BOTTOM);
-	    btnPause.setHorizontalTextPosition(AbstractButton.CENTER);
-		btnPause.setBounds(80, this.getHeight()-100, 70, 70);
-		contentPane.add(btnPause);
-		
-		btnForward = new JButton("Forward",new ImageIcon(PlayFrame.class.getResource("/se206_a03/png/next10.png")));
-		btnForward.setVerticalTextPosition(AbstractButton.BOTTOM);
-	    btnForward.setHorizontalTextPosition(AbstractButton.CENTER);
-		btnForward.setBounds(150,this.getHeight()-100, 70, 70);
-		contentPane.add(btnForward);
-		
-		btnBackward = new JButton("Backward",new ImageIcon(PlayFrame.class.getResource("/se206_a03/png/back12.png")));
-		btnBackward.setVerticalTextPosition(AbstractButton.BOTTOM);
-	    btnBackward.setHorizontalTextPosition(AbstractButton.CENTER);
-		btnBackward.setBounds(220,this.getHeight()-100, 70, 70);
-		contentPane.add(btnBackward);
+		Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
 		this.setVisible(true);
+		this.setTitle(Main.file.getName());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 400);
+		contentPane = new JPanel();
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		panel.setBounds(0,0, this.getWidth(), this.getHeight() - 100);
+		btnPlay = new JButton("Play", new ImageIcon(
+				PlayFrame.class.getResource("/se206_a03/png/play43.png")));
+		btnPlay.setBounds(10, this.getHeight() - 100, 70, 70);
+		btnPlay.setVerticalTextPosition(AbstractButton.BOTTOM);
+		btnPlay.setHorizontalTextPosition(AbstractButton.CENTER);
+		btnPlay.addActionListener(this);
+		contentPane.add(btnPlay);
+
+		btnPause = new JButton("Pause", new ImageIcon(
+				PlayFrame.class.getResource("/se206_a03/png/pause15.png")));
+		btnPause.setVerticalTextPosition(AbstractButton.BOTTOM);
+		btnPause.setHorizontalTextPosition(AbstractButton.CENTER);
+		btnPause.setBounds(90, this.getHeight() - 100, 70, 70);
+		btnPause.addActionListener(this);
+		contentPane.add(btnPause);
+
+		btnForward = new JButton("Forward", new ImageIcon(
+				PlayFrame.class.getResource("/se206_a03/png/next10.png")));
+		btnForward.setVerticalTextPosition(AbstractButton.BOTTOM);
+		btnForward.setHorizontalTextPosition(AbstractButton.CENTER);
+		btnForward.setBounds(170, this.getHeight() - 100, 70, 70);
+		btnForward.addActionListener(this);
+		contentPane.add(btnForward);
+
+		btnBackward = new JButton("Backward", new ImageIcon(
+				PlayFrame.class.getResource("/se206_a03/png/back12.png")));
+		btnBackward.setVerticalTextPosition(AbstractButton.BOTTOM);
+		btnBackward.setHorizontalTextPosition(AbstractButton.CENTER);
+		btnBackward.setBounds(260, this.getHeight() - 100, 70, 70);
+		btnBackward.addActionListener(this);
+		contentPane.add(btnBackward);
+
+		btnStrip = new JButton("Strip");
+		btnStrip.addActionListener(this);
+		btnStrip.setBounds(350, this.getHeight() - 100, 70, 70);
+		contentPane.add(btnStrip);
+
+		btnMute = new JButton("Mute");
+		btnMute.addActionListener(this);
+		btnMute.setBounds(440, this.getHeight() - 100, 70, 70);
+		contentPane.add(btnMute);
+
+		btnOverlay = new JButton("Overlay");
+		btnOverlay.addActionListener(this);
+		btnOverlay.setBounds(530, this.getHeight() - 100, 70, 70);
+		contentPane.add(btnOverlay);
+
+		btnReplace = new JButton("Replace");
+		btnReplace.addActionListener(this);
+		btnReplace.setBounds(620, this.getHeight() - 100, 80, 70);
+		contentPane.add(btnReplace);
+		
+		mediaPlayerComponent =new EmbeddedMediaPlayerComponent();
+		panel.setVisible(true);
+		panel.add(mediaPlayerComponent,BorderLayout.CENTER);
+		contentPane.add(panel);
+		setContentPane(contentPane);
+		//panel.setBackground(Color.black);
+		mediaPlayerComponent.getMediaPlayer().playMedia(Main.file.getAbsolutePath());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnPlay){
+			mediaPlayerComponent.getMediaPlayer().play();
+		}else if(e.getSource()==btnPause){
+			mediaPlayerComponent.getMediaPlayer().pause();
+		}else if(e.getSource()==btnForward){
+			//need to keep forward or backward
+			mediaPlayerComponent.getMediaPlayer().skip(10000);
+		}else if(e.getSource()==btnBackward){
+			mediaPlayerComponent.getMediaPlayer().skip(-10000);
+		}
+		else if(e.getSource()==btnMute){
+			mediaPlayerComponent.getMediaPlayer().mute();
+		}else if(e.getSource()==btnOverlay){
+			
+		}else if(e.getSource()==btnReplace){
+			
+		}else if(e.getSource()==btnStrip){
+			
+		}
 	}
 }
