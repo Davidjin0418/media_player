3a8e734c1f8f556f5e2fb770ea3b22006bfb0cbc
Davidjin0418 <bjin718@aucklanduni.ac.nz>
2014-10-21 21:15:30 +1300

    change package

---
+++ src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+import se206_a03.MainFrame;
+
+public class Main {
+	//create a static method to edit or play.(for other three frames to use)
+	 public static File file;
+	 public static void main(String[] args){
+		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	           public void run() {
+	               try {
+					createGUI();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	           }
+	       });
+	    } 
+		public static void createGUI() throws IOException{
+			MainFrame mainFrame=new MainFrame();
+		}
+}
